86abe13eca3ae95f9738c2fe1810118607b04a2d
Zhiyu Max Mao <zhuymao@gmail.com>
2017-02-15 22:37:00 -0800

    HW FINISHED

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,13 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, showproject);
+}
+function showproject(result) {
+	console.log(result);
+	var id = result['id'];
+	var detailHTML ='<p>' +result['title'] + '</p>' + '<p>' + result['date'] + '</p>' + '<img src="' + result['image'] + '" class="detailsImage">' + result['summary'];
+	$("#project" +id +" .details").html(detailHTML);
 }
 
 /*
@@ -35,4 +42,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", showcolor);
+}
+
+function showcolor(result) {
+	console.log(result);
+	var colors = result;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
